9ae3577225d1b3eec746fb547848e66241254276
Elnaam10 <e.umutoni@alustudent.com>
2024-05-31 12:55:33 +0200

    Spendwise

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,4 @@ void main() {
 class SpendWiseApp extends StatelessWidget {
   const SpendWiseApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-      theme: AppTheme.themeData,
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-// Spend wise
+ 
